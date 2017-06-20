fcedc7c6656a828c8d1dea5d840aec10b0a61be6
David LeGare <david.legare@scientificgames.com>
2017-06-20 10:12:42 -0500

    Make tests executable and actually use SauceLabs

--- tests/selenium_tests.js
+++ tests/selenium_tests.js
@@ -3,7 +3,14 @@ let webdriver = require('selenium-webdriver'),
     until = webdriver.until;
 
 let driver = new webdriver.Builder()
-    .forBrowser('firefox')
+    .usingServer('http://' + process.env.SAUCE_USERNAME + ':' + process.env.SAUCE_ACCESS_KEY + '@ondemand.saucelabs.com:80/wd/hub')
+    .withCapabilities({
+        'tunnel-identifier': process.env.TRAVIS_JOB_NUMBER,
+        build: process.env.TRAVIS_BUILD_NUMBER,
+        username: process.env.SAUCE_USERNAME,
+        accessKey: process.env.SAUCE_ACCESS_KEY,
+        browserName: "chrome"
+    })
     .build();
 
 driver.get('http://localhost:8000/');
